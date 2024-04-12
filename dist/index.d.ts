c1911d3937dec84757b661f497d4235eb66c8252
Ivan Fontosh <ifontosh@it-aces.com>
2024-04-12 08:03:15 +0800

    extnded interface IStorageFile

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -223,6 +223,9 @@ interface IFolderInTree {
 interface IStorageFile {
     bucket_name: string;
     object_name: string;
+    full_name: string;
+    updated_at: string;
+    is_file: boolean;
     size: number;
     url: string;
 }
--- src/interfaces/Storage/IStorageFile.ts
+++ src/interfaces/Storage/IStorageFile.ts
@@ -1,6 +1,9 @@
 export default interface IStorageFile {
 	bucket_name: string;
 	object_name: string;
+	full_name: string;
+	updated_at: string;
+	is_file: boolean;
 	size: number;
 	url: string;
 }
