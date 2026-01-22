ac5c13fdb86a41a8752d8b50386afc7aa419bb43
Ivan Fontosh <ifontosh@it-aces.com>
2026-01-22 18:24:46 +0800

    Update IUploadFileResponse interface

--- src/interfaces/Storage/IUploadFileResponse.ts
+++ src/interfaces/Storage/IUploadFileResponse.ts
@@ -2,5 +2,5 @@ import IStorageFile from './IStorageFile';
 import IStorageResponse from './IStorageResponse';
 
 export default interface IUploadFileResponse extends IStorageResponse {
-	data: IStorageFile;
+	data: IStorageFile[];
 }
